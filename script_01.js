055c7c6591be37f43db12dbedb8668589683d5a4
Daniela <Daniela-Haeussler@gmx.de>
2022-11-08 12:11:24 +0100

    Vor und Zuname 1

--- script_01.js
+++ script_01.js
@@ -10,9 +10,6 @@
 /***02-Deklaration & Wertzuweisung ***/
 
 let firstName="Max" //Wert Zuweisung
-console.log(firstName); //Ausgabe variable
-
 let lastName="Mütze";
-console.log(lastName);
 
-console.log(firstName + " "+ lastName);
+console.log(firstName + " "+ lastName);//Ausgabe variable
