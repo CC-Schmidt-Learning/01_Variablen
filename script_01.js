69c42c04d03d547c597400a8f1d40fa59c91d963
Daniela <Daniela-Haeussler@gmx.de>
2022-11-09 08:37:01 +0100

    gemeinsame Definition

--- script_01.js
+++ script_01.js
@@ -9,7 +9,12 @@
 */
 /***02-Deklaration & Wertzuweisung ***/
 
-let firstName="Max" //Wert Zuweisung
-let lastName="Mütze";
+//let firstName="Max" //Wert Zuweisung
+//let lastName="Mütze";
 
-console.log(firstName + " "+ lastName);//Ausgabe variable
+//console.log(firstName + " "+ lastName);//Ausgabe variable
+
+
+let firstName,lastName ="Max Mütze";
+
+console.log(firstName+lastName);
